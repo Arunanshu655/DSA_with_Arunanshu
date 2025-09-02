460ddae47d7d549441c257f517e6de9883ff4050
Arunanshu Dey <arunanshudey7@gmail.com>
2025-09-02 21:48:21 +0530

    Daily problem gfg : 01-09-2025

---
+++ GeeksForGeeks/DailyProblem/Swap_Kth_nodes_from_ends.java
@@ -0,0 +1,57 @@
+// Topics: Linked-List
+
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    public int findLength(Node head){
+        if(head==null) return 0;
+        Node temp = head;
+        int len = 0;
+        while(temp != null){
+            len++;
+            temp = temp.next;
+        }
+        return len;
+    }
+    public Node swapKth(Node head, int k) {
+        // code here
+        int n = findLength(head);
+        Node swap = null;
+        int idx = 1;
+        Node temp = head;
+        while(temp != null){
+            if(idx==k){
+                if(swap!=null){
+                    int t = swap.data;
+                    swap.data = temp.data;
+                    temp.data = t;
+                    break;
+                }else swap = temp;
+            }
+            else if(idx==(n-k+1)){
+                if(swap!=null){
+                    int t = swap.data;
+                    swap.data = temp.data;
+                    temp.data = t;
+                    break;
+                }else swap = temp;
+            }
+            temp = temp.next;
+            idx++;
+        }
+        return head;
+    }
+}
+
+
+// Time Complexity: O(n)
+// Auxiliary Space: O(1)
