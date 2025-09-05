e8629c783d41337bcab48a5a607304de98379662
Arunanshu Dey <arunanshudey7@gmail.com>
2025-09-05 11:29:55 +0530

    Daily problem gfg : 05-08-2025

---
+++ GeeksForGeeks/DailyProblem/Sort_a_linked_list_of_0s,_1s_and_2s.java
@@ -0,0 +1,46 @@
+// Topics: Linked-List
+
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}*/
+
+class Solution {
+    public Node segregate(Node head) {
+        // code here
+        if (head == null || head.next == null) return head;
+        Node zeroD = new Node(-1), oneD = new Node(-1), twoD = new Node(-1);
+        Node zero = zeroD, one = oneD, two = twoD;
+        
+        Node curr = head;
+        while(curr!=null){
+            if(curr.data==0){
+                zero.next = curr;
+                zero = zero.next;
+            }else if(curr.data == 1){
+                one.next = curr;
+                one = one.next;
+            }else{
+                two.next = curr;
+                two = two.next;
+            }
+            curr = curr.next;
+        }
+        
+        zero.next = (oneD.next != null)? oneD.next : twoD.next;
+        one.next = twoD.next;
+        two.next = null;
+        
+        return zeroD.next;
+    }
+}
+
+// Time Complexity: O(n)
+// Auxiliary Space: O(1)
