1e03eea44fdab36a6f0cbad746fed5be492d90ff
Arunanshu Dey <arunanshudey7@gmail.com>
2025-09-22 00:19:44 +0530

    Daily Problem LC: 21-09-2025

---
+++ LeetCode/Algorithm/BitManipulation/137.Single_Number_II.java
@@ -0,0 +1,16 @@
+// Topics: Bit manipulation
+
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0;
+        for(int num : nums){
+            ones = (ones ^ num) & (~twos);
+            twos = (twos ^ num) & (~ones);
+        }
+        return ones;
+    }
+}
+
+// Time Complexity: O(n)
+// Auxiliary Space: O(1)
