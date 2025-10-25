8654c415748424624cbc66ee06eee6bda97a5b9c
Arunanshu Dey <arunanshudey7@gmail.com>
2025-10-25 10:58:30 +0530

    Daily problem gfg : 24-10-2025

---
+++ GeeksForGeeks/DailyProblem/Split_Array_Subsequences.java
@@ -0,0 +1,30 @@
+// Topics: Hash-Map
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> kClosest(int[][] points, int k) {
+        // code here
+        Map<int[],Double> map = new HashMap<>();
+        for(int[] point : points){
+            map.put(point, Math.sqrt(Math.pow(point[0],2) + Math.pow(point[1],2)));
+        }
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
+            return ((map.get(a)-map.get(b))>0?1:-1);
+        });
+        for(int[] point: points) pq.add(point);
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            ArrayList<Integer> temp = new ArrayList<>();
+            temp.add(curr[0]);
+            temp.add(curr[1]);
+            ans.add(temp);
+            if(ans.size()==k) break;
+        }
+        
+        return ans;
+    }
+}
+
+// Time Complexity: O(n log n)
+// Auxiliary Space: O(n)
